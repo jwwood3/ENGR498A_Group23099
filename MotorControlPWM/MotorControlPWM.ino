6cc27d645a7cd2c16e9a78b3cb2c08b4a6ac15c9
jwwood3 <jacksonwood1301@gmail.com>
2022-11-08 15:34:53 -0700

    Update parameter formatting to better facilitate expansion

--- MotorControlPWM/MotorControlPWM.ino
+++ MotorControlPWM/MotorControlPWM.ino
@@ -4,7 +4,7 @@ const int NUM_MOTORS = 9;
 const int numChars = 2420;
 char receivedChars[numChars];
 char tempChars[numChars];
-float* receivedData;
+float receivedData[420];
 boolean newData = false;
 
 //Analog Position Scaling
@@ -12,7 +12,7 @@ int analogMax = 1023; // Maximum scale input value from the potentiometer
 float target[NUM_MOTORS]; // Target position value in cm
 double maxCM = 10.16; // Maximum position value in cm
 double conversion = (1.0*analogMax)/(1.0*maxCM); // conversion rate from input values to cm
-int moe = 8; // Acceptable error
+int moe = 20; // Acceptable error
 
 //PINS
 int analogs[NUM_MOTORS];
@@ -21,9 +21,11 @@ int controlPin1s[NUM_MOTORS];
 int controlPin2s[NUM_MOTORS];
 
 //Motor Data
+int viewMotor = 0;
+int pwmVals[NUM_MOTORS];
 int curPos[NUM_MOTORS];
 int MAX_PWM=255;
-int MIN_PWM=180;
+int MIN_PWM=255;
 
 //PID
 float error[NUM_MOTORS];
@@ -32,20 +34,34 @@ float integ[NUM_MOTORS];
 float deriv[NUM_MOTORS];
 float control[NUM_MOTORS];
 
-float kp = 3;
-float kd = 0.02;
-float ki = 4.0;
+float kp = 5;
+float kd = 0;
+float ki = 0;
 
 float curTime;
 float prevTime=-1.0;
 float deltaTime;
 
+/* Checked ports
+
+Good Ports
+PWM: 13,12
+Analog: 0,2,3
+
+Bad Ports
+PWM: 11
+Analog: 1
+*/
+
 void setup() {
+  int pwmPortOrder[] = {2,3,4,5,6,7,8,9,10,11,12,13};
+  int analogPortOrder[] = {0,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
   //Initialize motor count dependent arrays
   for(int i=0;i<NUM_MOTORS;i++){
     target[i]=5.0;    
-    PWMS[i]=2+i;
-    analogs[i]=i;
+    PWMS[i]=pwmPortOrder[i];
+    pwmVals[i]=0;
+    analogs[i]=analogPortOrder[i];
     controlPin1s[i]=52-(2*i);
     controlPin2s[i]=53-(2*i);
     integ[i]=0.0;
@@ -69,23 +85,27 @@ void loop() {
   if(newData == true){
     strcpy(tempChars, receivedChars);
     int l=parseData();
-    if(l==NUM_MOTORS){// If vals are given for all motors, set all targets to new values
+    if(l==3 && fEqual(receivedData[0],-1.0)){ // If 3 floats are given and the f1=-1,
+      if(fEqual(receivedData[1],-1.0)){ // If f2=-1, set viewMotor to f3
+        viewMotor = (int)receivedData[2];
+      } else { // else assign target f3 to motor f2
+        int i = (int)(receivedData[1]);            // set target[f2]=f3
+        for(int j=0;j<l;j++){
+          Serial.print(j);
+          Serial.print(": ");
+          Serial.println(receivedData[j]);
+        }
+        if(i<NUM_MOTORS && i>=0){
+          target[i]=receivedData[2];
+          Serial.print("New Target for motor ");
+          Serial.print(i);
+          Serial.print(": ");
+          Serial.println(target[i]);
+        }
+      }
+    } else if(l==NUM_MOTORS){// If vals are given for all motors, set all targets to new values
       for(int i=0;i<l;i++){
         target[i] = receivedData[i];
-        Serial.print("New Target for motor ");
-        Serial.print(i);
-        Serial.print(": ");
-        Serial.println(target[i]);
-      }
-    } else if(l==3 && fEqual(receivedData[0],-1.0)){ // If 3 floats are given and the f1=-1,
-      int i = (int)(receivedData[1]);            // set target[f2]=f3
-      for(int j=0;j<l;j++){
-        Serial.print(j);
-        Serial.print(": ");
-        Serial.println(receivedData[j]);
-      }
-      if(i<NUM_MOTORS && i>=0){
-        target[i]=receivedData[2];
         Serial.print("New Target for motor ");
         Serial.print(i);
         Serial.print(": ");
@@ -150,8 +170,6 @@ int parseData() {
 
   strtokIndx = strtok(tempChars,",");
   int packageSize = atoi(strtokIndx);
-  float dat[packageSize];
-  receivedData = dat;
   for(int i=0;i<packageSize;i++){
     strtokIndx = strtok(NULL,",");
     receivedData[i] = atof(strtokIndx);
@@ -197,6 +215,8 @@ void motors(){
     }
     if(pwmVal<MIN_PWM && fabs(error[i])>moe){
       pwmVal = MIN_PWM;
+    } else if(pwmVal<MIN_PWM){
+      pwmVal=0;
     }
     if(dir==-1){
       digitalWrite(controlPin1s[i],LOW);
@@ -209,6 +229,7 @@ void motors(){
       digitalWrite(controlPin2s[i],LOW);
       pwmVal = 0;
     }
+    pwmVals[i] = pwmVal;
     analogWrite(PWMS[i],pwmVal);
   }
 }
@@ -220,13 +241,15 @@ bool fEqual(float f1,float f2){
 
 void debug(){
   Serial.print("target: ");
-  Serial.print(int(target[0]*conversion));
+  Serial.print(int(target[viewMotor]*conversion));
   Serial.print("\t|\tpos: ");
-  Serial.print(curPos[0]);
+  Serial.print(curPos[viewMotor]);
   Serial.print("\t|\tError: ");
-  Serial.print(error[0]);
+  Serial.print(error[viewMotor]);
   Serial.print("\t|\tDeriv: ");
-  Serial.print(deriv[0]);
+  Serial.print(deriv[viewMotor]);
   Serial.print("\t|\tInteg: ");
-  Serial.println(integ[0]);
+  Serial.print(integ[viewMotor]);
+  Serial.print("\t|\tPWM: ");
+  Serial.println(pwmVals[viewMotor]);
 }
